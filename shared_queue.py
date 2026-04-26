0c71298b36c43988ce625abf2aaec39bb660e56c
Anant Sharma <anant.sharma.dev@gmail.com>
2026-04-26 20:48:05 +0530

    Delete shared_queue.py

--- shared_queue.py
+++
@@ -1,5 +0,0 @@
-import queue
-
-# This file holds the single, shared UI update queue.
-# Both main.py and brain.py will import this file to access the same queue object.
-ui_update_queue = queue.Queue()
